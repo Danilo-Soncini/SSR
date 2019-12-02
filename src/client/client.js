2bb6cc3392d7a53c786677ab427733acdbc31523
Danilo Dantas Soncini <danilo.soncini@outlook.com>
2019-12-02 18:06:06 -0300

    webpack merge

--- src/client/client.js
+++ src/client/client.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home';
 
-ReactDOM.hydrate(<Home/>, document.querySelector('#root'));
+ReactDOM.hydrate(
+  <Home></Home>, 
+  document.querySelector('#root')
+);
