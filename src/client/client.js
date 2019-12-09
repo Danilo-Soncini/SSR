6591bb3a3363edc857615c87f1fcc207b599074b
Danilo Dantas Soncini <danilo.soncini@outlook.com>
2019-12-09 13:24:52 -0300

    SSR add reduce and API request

--- src/client/client.js
+++ src/client/client.js
@@ -1,11 +1,20 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import reduceres from './reducers';
+
+const store = createStore(reduceres, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>, 
   document.querySelector('#root')
 ); 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import 'babel-polyfill';
 import express from 'express';
-import renderer from './helpers/renderer'
+import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
+
 const PORT = 9841;
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+
+  res.send(renderer(req, store));
 })
 
 app.listen(PORT, () => {
